d604cb742849eceb0ae6998c3a5b69320d291dee
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2023-10-10 18:37:29 +0300

    Disable tow defalut values in `useQuery` options Set `refetchOnWindowFocus` and `refetchOnMount` properties to 'false' in the options object of `useQuery` hook from '@tanstack/react-query' library.
    - Change the extention of `page.js` file in '/emails' path to be `page.tsx`.

---
+++ src/app/emails/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function Emails() {
+
+    return (
+        <Container maxWidth="md">
+
+        </Container>
+    )
+}
--- src/hooks/useGetApi.ts
+++ src/hooks/useGetApi.ts
@@ -1,12 +1,22 @@
-import { useQuery } from '@tanstack/react-query'
+import { QueryKey, UseQueryOptions, useQuery } from '@tanstack/react-query'
 import host from '@/CONSTANT/API_hostName';
 import useApiRequest from './useApiRequest';
 
-interface params { key: string[], path: string, moreProps?: {} }
+interface params {
+    key: string[],
+    path: string,
+    moreProps?: UseQueryOptions<any, any, any, QueryKey>
+}
 
 export default function useGetApi(props: params) {
     const { api } = useApiRequest();
     const { key, path, moreProps } = props;
     async function theFunc() { return (await api.get(`${host}${path}`)).data }
-    return useQuery({ queryKey: key, queryFn: theFunc, ...moreProps })
+    return useQuery({
+        queryKey: key,
+        queryFn: theFunc,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        ...moreProps
+    })
 }
