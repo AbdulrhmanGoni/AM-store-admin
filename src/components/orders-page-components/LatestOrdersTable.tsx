82b8b631921309c0fbb46b4920e55228cd2684e1
Abdulrhman <abdulrhmangoni@gmail.com>
2024-01-15 00:32:43 +0300

    Create a hook for dealing with orders data separately Create `useLatestOrders` to move the query that gets the latest order from the ui component `LatestOrdersTable` into it, And create inside it watching new orders logic using `useEventSource` for receive any new order created from the server immediately

--- src/components/orders-page-components/LatestOrdersTable.tsx
+++ src/components/orders-page-components/LatestOrdersTable.tsx
@@ -1,35 +1,22 @@
 import { Paper, useMediaQuery, LinearProgress } from '@mui/material';
-import { DataGrid, GridRowsProp, useGridApiRef } from '@mui/x-data-grid';
-import columns from './OrdersGridColumnsConfig';
+import { DataGrid } from '@mui/x-data-grid';
 import { IllustrationCard } from '@abdulrhmangoni/am-store-library';
-import useGetApi from '../../hooks/useGetApi';
-
-interface LatestOrdersType {
-    totalPrice: number,
-    products: string[],
-    state: string,
-    expectedDeliveryDate: string,
-    deliveryPrice: number,
-    createdAt: string,
-    userData: GridRowsProp
-}
+import useLatestOrders from '../../hooks/useLatestOrders';
 
 export default function LatestOrdersTable() {
 
     const paddingSpace = useMediaQuery("(min-width: 900px)") ? 35 : 19;
-    const apiRef = useGridApiRef();
 
-    const query = "latest-orders"
-    const { data: orders = [], isLoading } = useGetApi<readonly LatestOrdersType[]>({
-        key: [query],
-        path: `orders/${query}?limit=10`
-    })
+    const {
+        orders,
+        columns,
+        isLoading
+    } = useLatestOrders();
 
     return (
         <Paper sx={{ width: `calc(100vw - ${paddingSpace}px)` }}>
             <DataGrid
                 sx={{ height: "796px" }}
-                apiRef={apiRef}
                 getRowId={(row) => { return row._id }}
                 rows={orders}
                 columns={columns}
---
+++ src/hooks/useLatestOrders.ts
@@ -0,0 +1,47 @@
+import { GridRowsProp } from "@mui/x-data-grid"
+import useGetApi from "./useGetApi"
+import columns from "../components/orders-page-components/OrdersGridColumnsConfig"
+import useEventSource from "./useEventSource"
+import { useEffect } from "react"
+import { useQueryClient } from "@tanstack/react-query"
+
+interface LatestOrdersType {
+    totalPrice: number,
+    products: string[],
+    state: string,
+    expectedDeliveryDate: string,
+    deliveryPrice: number,
+    createdAt: string,
+    userData: GridRowsProp
+}
+
+export default function useLatestOrders() {
+
+    const query = "latest-orders"
+    const { data: orders = [], isLoading } = useGetApi<readonly LatestOrdersType[]>({
+        key: [query],
+        path: `orders/${query}?limit=10`
+    })
+    const queryClient = useQueryClient()
+
+    const latestOrders = useEventSource("orders/watch-new-orders");
+
+    function handleNewOrder(event: MessageEvent<string>) {
+        if (event.data) {
+            queryClient.setQueryData<readonly LatestOrdersType[]>([query], (state) => {
+                return [JSON.parse(event.data) as LatestOrdersType, ...state!]
+            })
+        }
+    }
+
+    useEffect(() => {
+        const eventSource = latestOrders();
+        eventSource.addEventListener("message", handleNewOrder)
+    }, [])
+
+    return {
+        orders,
+        columns,
+        isLoading
+    }
+}
