b2d06772296837f8df1da841dfe3572170603c55
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-03-03 17:15:24 +0300

    Replace the random logo with project't actual logo
    Update `@abdulrhmangoni/am-store-library` library and use `AMLogo`
    component that came with the new update in the header of the app
    (`AdminBar`) as the logo of "AM Store"

--- src/components/AdminBar.tsx
+++ src/components/AdminBar.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Menu } from '@mui/icons-material';
-import { Avatar, IconButton, Toolbar, Box, AppBar } from '@mui/material';
+import { IconButton, Toolbar, Box, AppBar } from '@mui/material';
 import AdminSideBar from './AdminSideBar';
-import { P } from "@abdulrhmangoni/am-store-library";
+import { AMLogo } from "@abdulrhmangoni/am-store-library";
 import NotificationsCenter from "./NotificationsCenter";
 
 export default function AdminAppBar() {
@@ -15,18 +15,7 @@ export default function AdminAppBar() {
             <AppBar position='relative'>
                 <Toolbar>
                     <IconButton onClick={() => { setDrawerState(true) }} ><Menu /></IconButton>
-                    <IconButton
-                        sx={{ width: 50, height: 50, p: 0, mr: 1 }}>
-                        <Avatar src="https://live.staticflickr.com/65535/52735512062_1fb9bc6f2a_o.jpg">AM</Avatar>
-                    </IconButton>
-                    <P
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ display: { xs: 'none', sm: 'block' }, fontWeight: "bold" }}
-                    >
-                        AM STORE
-                    </P>
+                    <AMLogo />
                     <Box sx={{ flexGrow: 1 }} />
                     <NotificationsCenter />
                 </Toolbar>
