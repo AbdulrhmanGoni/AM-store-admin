b3b52bc3641480eba50ad944b817a09bd481973a
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-04-17 17:23:05 +0300

    ui: make `NotFoundPage` component fills the whole page.
    Use the prop `fullPage` of `IllustrationCard` component inside `NotFoundPage` component
    to make the component fills the whole page.
    
    Set "Back" and "Home Page" buttons size in `NotFoundPage` component to "small"
    
    Rename `back` function in `NotFoundPage` component  to `navigate`

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,21 +1,21 @@
 import { IllustrationCard } from "@abdulrhmangoni/am-store-library";
-import { Box, Button, useMediaQuery } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export default function NotFoundPage() {
-    const media = useMediaQuery("(max-width: 599px)");
     const { pathname } = useLocation();
-    const back = useNavigate();
+    const navigate = useNavigate();
     return (
         <IllustrationCard
             title="Not Found Page"
             alertType="info"
             message={`There is no page at this path '${pathname}'`}
             illustratorType="notFound"
+            fullPage
         >
             <Box className="flex-row-center" gap={2}>
-                <Button size={media ? "small" : "large"} onClick={() => back(-1)} variant="contained">Back</Button>
-                <Button size={media ? "small" : "large"} variant="contained">Do Something</Button>
+                <Button size="small" onClick={() => navigate(-1)} variant="contained">Back</Button>
+                <Button size="small" onClick={() => navigate("/")} variant="contained">Home Page</Button>
             </Box>
         </IllustrationCard>
     )
